2eb4ffd4e24e951d2b253aa581cef7a58874a6dd
Maurya <Prabhat.Maurya@allscripts.com>
2019-03-30 10:32:49 +0530

    First commit

---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly:OwinStartup(typeof(WebApplication3.Startup))]
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            Console.WriteLine("Start up called!!");
+        }
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
